7d02ba6107326732ab095335e9a0f686268dbe2d
SadiMohammad <sadimohammad97@gmail.com>
2019-05-20 13:20:10 +0600

    dataload checked but problem with mask

--- code/dataLoader.py
+++ code/dataLoader.py
@@ -5,24 +5,28 @@ import cv2
 import glob
 
 class DataLoad:
-    def __init__(self, dirImage, img_rows, img_cols, dtype = np.uint8):
+    def __init__(self, dirImage, img_rows, img_cols, raw = True, dtype = np.uint8):
         self.dirImage = dirImage
         self.img_rows = img_rows
-        self.img_cols = image_cols
+        self.img_cols = img_cols
         self.dtype = dtype
+        self.data = raw
 
     def loadPathData(self):
         filenames= []
-        filenames += glob.glob(dirImage + "/*" + ".jpg")
+        if self.data:
+            filenames += glob.glob(self.dirImage + "/*" + ".jpg")
+        else:
+            filenames += glob.glob(self.dirImage + "/*" + ".tif")
         images = []
         for file in filenames:
             img = cv2.imread(file,0)
-            img= cv2.resize(img, (img_rows, img_cols), interpolation = cv2.INTER_AREA)
+            img= cv2.resize(img, (self.img_rows, self.img_cols), interpolation = cv2.INTER_AREA)
             img = np.asarray(img, dtype = self.dtype)
             images.append(img)
         images = np.asarray(images, dtype = self.dtype)
         print(np.shape(images))
-        images = images.reshape(-1, 1, img_rows, img_cols)
+        images = images.reshape(-1, 1, self.img_rows, self.img_cols)
         print(np.shape(images))
         return images
 
