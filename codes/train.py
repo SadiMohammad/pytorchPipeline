014c69778698f66f4c495a73428b74fa79aef82e
SadiMohammad <sadimohammad97@gmail.com>
2019-10-25 18:22:45 +0600

    config-arg combined

--- codes/train.py
+++ codes/train.py
@@ -17,43 +17,43 @@ from losses import *
 sys.path.append('..')
 # from models import UNet
 # from models import CleanU_Net
-# from modeling import DeepLab
 from models import LungNet
 
 
 class config:
-    def __init__(self):
+    def __init__(self, scheme):
         # parser config
+        self.scheme = scheme
+        print(scheme)
+        print(type(scheme))
         config_file = "./config.ini"
         parser = ConfigParser()
         parser.read(config_file)
 
-        # default config
-        self.trainImagePath = parser["DEFAULT"].get("trainImagePath")
-        self.trainMaskPath = parser["DEFAULT"].get("trainMaskPath")
-        self.checkpointsPath = parser["DEFAULT"].get("checkpointsPath")
-
-        # train config
-        self.learningRate = parser["TRAIN"].getfloat("learningRate")
-        self.trainRatio = parser["TRAIN"].getfloat("trainRatio")
-        self.optimizer = parser["TRAIN"].get("optimizer")
-        self.loss = parser["TRAIN"].get("loss")
-        self.size = parser["TRAIN"].getint("size")
-        self.epochs = parser["TRAIN"].getint("epochs")
-        self.batchSize = parser["TRAIN"].getint("batchSize")
-        self.modelWeightLoad = parser["TRAIN"].getboolean("modelWeightLoad")
-        self.modelWeight = parser["TRAIN"].get("modelWeight")
-        self.saveBestModel = parser["TRAIN"].getboolean("saveBestModel")
+        # config
+        self.imagesPath = parser[self.scheme].get("imagesPath")
+        self.masksPath = parser[self.scheme].get("masksPath")
+        self.checkpointsPath = parser[self.scheme].get("checkpointsPath")
+        self.learningRate = parser[self.scheme].getfloat("learningRate")
+        self.trainRatio = parser[self.scheme].getfloat("trainRatio")
+        self.optimizer = parser[self.scheme].get("optimizer")
+        self.loss = parser[self.scheme].get("loss")
+        self.size = parser[self.scheme].getint("size")
+        self.epochs = parser[self.scheme].getint("epochs")
+        self.batchSize = parser[self.scheme].getint("batchSize")
+        self.modelWeightLoad = parser[self.scheme].getboolean("modelWeightLoad")
+        self.modelWeight = parser[self.scheme].get("modelWeight")
+        self.saveBestModel = parser[self.scheme].getboolean("saveBestModel")
 
 
 class train(config):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, scheme):
+        super().__init__(scheme)
 
     def main(self, model, device):
         modelName = model.__class__.__name__
-        imageFiles = glob.glob(self.trainImagePath + "/*" + ".jpg")
-        maskFiles = glob.glob(self.trainMaskPath + "/*" + ".tif")
+        imageFiles = glob.glob(self.imagesPath + "/*" + ".jpg")
+        maskFiles = glob.glob(self.masksPath + "/*" + ".tif")
 
         imgTrain = imageFiles[:int(len(imageFiles) * self.trainRatio)]
         imgVal = imageFiles[int(len(imageFiles) * self.trainRatio):]
@@ -102,6 +102,8 @@ class train(config):
                 trueMasks = sample_train[1].to(device)
                 preds = model(images)
                 predMasks = preds
+
+                # for deeplabv3
                 # preds = preds['out'] 
                 # predMasks = torch.sigmoid(preds) 
                 # print(predMasks.values())
@@ -149,7 +151,7 @@ if __name__ == "__main__":
     args = argParser.parse_args()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = LungNet(1, 1).to(device)
-    # checkpoint = torch.load(train().checkpointsPath + '/' + modelName + '/' + '2019-08-01 04:39:48.331969_epoch-10_dice-0.7213704585097731.pth')
+    # checkpoint = torch.load(train(args.config_scheme).checkpointsPath + '/' + modelName + '/' + '2019-08-01 04:39:48.331969_epoch-10_dice-0.7213704585097731.pth')
     # model.load_state_dict(checkpoint['model_state_dict'])
 	# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 	# epoch = checkpoint['epoch']
@@ -157,18 +159,18 @@ if __name__ == "__main__":
     # model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=False, num_classes=1)
     modelName = model.__class__.__name__
     model = model.to(device)
-    checkpoint = torch.load(train().checkpointsPath + '/' + modelName + '/' + train().modelWeight)
+    checkpoint = torch.load(train(args.config_scheme).checkpointsPath + '/' + modelName + '/' + train(args.config_scheme).modelWeight)
     model.load_state_dict(checkpoint['model_state_dict'])
     try:
         # Create model Directory
-        checkpointDir = train().checkpointsPath + '/' + modelName
+        checkpointDir = train(args.config_scheme).checkpointsPath + '/' + modelName
         if not (os.path.exists(checkpointDir)):
             os.mkdir(checkpointDir)
             print("\nDirectory ", modelName, " Created \n")
-        train().main(model, device)
+        train(args.config_scheme).main(model, device)
     except KeyboardInterrupt:
         torch.save(model.state_dict(),
-                   train().checkpointsPath + '/' + model.__class__.__name__ + '/' + 'INTERRUPTED.pth')
+                   train(args.config_scheme).checkpointsPath + '/' + model.__class__.__name__ + '/' + 'INTERRUPTED.pth')
         print('Saved interrupt')
         try:
             sys.exit(0)
