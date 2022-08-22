ebfb40b468013d64262ab3a719e3941564c7029d
Sadi Mohammad Siddiquee <sadimohammad97@gmail.com>
2022-08-23 05:19:55 +0600

    train.py added

--- train.py
+++ train.py
@@ -1,55 +1,112 @@
-# from models.model import Model
-# import torch
-# from utils.config import Config
-# import argparse
-# arg_parser = argparse.ArgumentParser()
-# arg_parser.add_argument(
-#     "--config_file", help="config file name/experiment name", required=True
-# )
-# args = arg_parser.parse_args()
-# Cfgs = Config(args)
-# cfgs = Cfgs.cfgs
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# model = Model(cfgs).get_model()
-# print(model)
+import time
+import torch
+import os, sys, random, string
+import argparse
+import wandb
+from torch.utils.data import DataLoader
+from torch import optim
+import torchvision.transforms as transforms
+from models.model import Model
+from configs.config import Config
+from utils.logger import Logger
+from dataloaders.dataloader import Dataset_ROM, Dataset_RAM
+from trainer import Trainer
 
-# import torchvision.transforms as transforms
-# from torch.utils.data import DataLoader
-# from tqdm import tqdm
-# import torch
-# from utils.config import Config
-# from dataloaders import Dataset_ROM
-# import argparse
+time_stamp = time.strftime("%Y_%m_%d_%H_%M_%S")
 
-# arg_parser = argparse.ArgumentParser()
-# arg_parser.add_argument(
-#     "--config_file", help="config file name/experiment name", required=True
-# )
-# args = arg_parser.parse_args()
-# Cfgs = Config(args)
-# cfgs = Cfgs.cfgs
 
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# transformers = {
-#     "image": transforms.Compose(
-#         [
-#             transforms.Resize((224, 224)),
-#             transforms.ToTensor(),
-#             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-#         ]
-#     ),
-#     "gt": transforms.Compose(
-#         [
-#             transforms.Resize((224, 224)),
-#             transforms.ToTensor(),
-#         ]
-#     ),
-# }
+def main(Cfgs):
+    cfgs = Cfgs.cfgs
+    device = torch.device(cfgs["train_setup"]["device"])
 
-# dataset_train = Dataset_ROM(cfgs, transformers=transformers)
-# loader_train = DataLoader(dataset_train, batch_size=4, shuffle=True)
-# for i_train, sample_train in enumerate(tqdm(loader_train)):
-#     images = sample_train[0]
-#     labels = sample_train[1]
-#     print(images.size())
-#     break
+    # LOGGING
+    if cfgs["logs"]["save_local_logs"]:
+        sys.stdout = Logger(
+            os.path.join(
+                cfgs["logs"]["local_logs_path"],
+                cfgs["experiment_name"],
+                "{}.log".format(time_stamp),
+            )
+        )
+    if cfgs["logs"]["use_wandb"]:
+        run_name = (
+            cfgs["experiment_name"]
+            + "-"
+            + "".join(random.choices(string.ascii_lowercase, k=5))
+        )
+        wandb.init(
+            project=cfgs["logs"]["pytorchPipeline"],
+            entity=cfgs["logs"]["wandb_entity"],
+            config=Cfgs.cfgs_single_dict,
+            name=run_name,
+        )
+
+    # DATA LOADERS
+    transformers = {
+        "image": transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=0.485, std=0.229),
+            ]
+        ),
+        "gt": transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+            ]
+        ),
+    }
+    dataset_train = Dataset_ROM(cfgs, transformers=transformers)
+    loader_train = DataLoader(
+        dataset_train, batch_size=cfgs["train_setup"]["batch_size"], shuffle=True
+    )
+    dataset_valid = Dataset_ROM(cfgs, transformers=transformers)
+    loader_valid = DataLoader(
+        dataset_valid, batch_size=cfgs["train_setup"]["batch_size"], shuffle=False
+    )
+
+    # MODEL
+    model = Model(cfgs).get_model()
+    model = model.to(device)
+    if cfgs["logs"]["use_wandb"]:
+        wandb.watch(model)
+
+    # Training
+    optimizer = getattr(optim, cfgs["optimizer"]["optimizer_fn"])(
+        model.parameters(),  # momentum=0.9,  # for sgd
+        lr=cfgs["optimizer"]["initial_lr"],
+        weight_decay=0.0005,
+    )
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=cfgs["optimizer"]["optimizer_fn"],
+        betas=(0.9, 0.999),
+        eps=1e-08,
+        weight_decay=0,
+        amsgrad=False,
+    )
+    loss_fn = cfgs["train_setup"]["loss"]
+    metric_fn = cfgs["train_setup"]["metric"]
+    Trainer(
+        time_stamp,
+        model,
+        optimizer,
+        device,
+        loader_train,
+        loader_valid,
+        loss_fn,
+        metric_fn,
+    )
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "--config_file",
+        help="config file name/experiment name",
+        default="train",
+    )
+    args = arg_parser.parse_args()
+    Cfgs = Config(args)
+    main(Cfgs)
--- trainer.py
+++ trainer.py
@@ -1,6 +1,9 @@
 import os
 import torch
+import wandb
 from tqdm import tqdm
+from utils.loss import Loss
+from utils.metric import Metric
 
 
 class Trainer:
@@ -9,29 +12,41 @@ class Trainer:
     ):  # time_stamp, model, optimizer, device, loader_train, loader_valid, loss_fn, metric_fn
         self.__dict__.update(kwargs)
 
-    def train_epoch(self):
+    def train(self):
         self.optimizer.zero_grad()
         best_valid_score = self.cfgs["train_setup"]["best_valid_score"]
         for epoch in range(self.cfgs["train_setup"]["epochs"]):
             print("\tStarting epoch - {}/{}.".format(epoch + 1, self.epochs))
             self.model.train()
+            total_batch_loss = 0
 
             for i_train, sample_train in enumerate(tqdm(self.loader_train)):
-                images = sample_train[0]
-                labels = sample_train[1]
+                images = sample_train[0].to(self.device)
+                labels = sample_train[1].to(self.device)
                 preds = self.model(images)
                 if self.cfgs["model"]["model_name"].lower() == "deeplabv3":
                     preds = preds["out"]
                     preds = torch.sigmoid(preds)
 
-                m_batch_loss = self.loss_fn(preds, labels)
-                m_batch_metric = torch.mean(self.metric_fn(preds, labels))
+                m_batch_loss = getattr(Loss(labels, preds), self.loss_fn)()
+                m_batch_metric = torch.mean(
+                    getattr(Metric(labels, preds), self.metric_fn)()
+                )
+                total_batch_loss+=(torch.mean(m_batch_loss)).item()
 
                 m_batch_loss.backward()
                 self.optimizer.step()
                 self.optimizer.zero_grad()
 
             epoch_validation_metric = self._validate(epoch)
+            if self.cfgs["logs"]["use_wandb"]:
+                wandb.log(
+                    {
+                        "epoch_mean_train_loss": total_batch_loss/(i_train+1),
+                        "epoch_validation_metric": epoch_validation_metric,
+                        "mini_batch_metric": m_batch_metric.item(),
+                    }
+                )
             if epoch_validation_metric > best_valid_score:
                 best_valid_score = epoch_validation_metric
                 if self.cfgs["train_setup"]["save_best_model"]:
@@ -57,15 +72,17 @@ class Trainer:
         epoch_valid_metric = 0
         with torch.no_grad():
             for i_valid, sample_valid in enumerate(self.loader_valid):
-                images = sample_valid[0]
-                labels = sample_valid[1]
+                images = sample_valid[0].to(self.device)
+                labels = sample_valid[1].to(self.device)
                 preds = self.model(images)
                 if self.cfgs["model"]["model_name"].lower() == "deeplabv3":
                     preds = preds["out"]
                     preds = torch.sigmoid(preds)
 
-                m_batch_loss = self.loss_fn(preds, labels)
-                m_batch_metric = torch.mean(self.metric_fn(preds, labels))
+                m_batch_loss = getattr(Loss(labels, preds), self.loss_fn)()
+                m_batch_metric = torch.mean(
+                    getattr(Metric(labels, preds), self.metric_fn)()
+                )
                 if self.cfgs["train_setup"]["use_thld_for_valid"]:
                     preds = (preds > self.cfgs["train_setup"]["thld_for_valid"]).float()
                 epoch_valid_metric += m_batch_metric.item()
